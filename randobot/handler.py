a4ab0ac7725f19904fddf1c1830d3467ca1fdf13
Brian Williams <brianwilliams42@gmail.com>
2021-07-05 22:31:26 -0700

    oops fixed?

--- randobot/handler.py
+++ randobot/handler.py
@@ -128,7 +128,7 @@ class RandoHandler(RaceHandler):
         await self.send_message('New seed with flags {} GLHF!'.format(flags))
 
     async def update_info(self):
-        await self.set_raceinfo('Randomizer {} Seed {} Flags {}'.format(race_version, self.race_seed, self.race_flagstring), overwrite=True)
+        await self.set_raceinfo('Randomizer {} Seed {} Flags {}'.format(self.race_version, self.race_seed, self.race_flagstring), overwrite=True)
 
     def _race_in_progress(self):
         return self.data.get('status').get('value') in ('pending', 'in_progress')
