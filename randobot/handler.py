fb476a942bce5d7e41efdb8e190020d3c799d2a6
brianwilliams42 <brianwilliams42@gmail.com>
2021-12-10 22:49:18 -0800

    Adding 3.0 commands (#3)

--- randobot/handler.py
+++ randobot/handler.py
@@ -20,11 +20,11 @@ class RandoHandler(RaceHandler):
             goal_name = self.data.get('goal', {}).get('name')
             if (goal_name == 'Standard Flags' or goal_name == 'Tournament'):
                 await self.send_message(
-                    'Welcome to DWR! Create a standard seed with !roll'
+                    'Welcome to DWR! Create a standard seed with !roll or !roll3 for version 3.0'
                 )
             else:
                 await self.send_message(
-                    'Welcome to DWR! Create a custom flag seed with !dwflags <flags>'
+                    'Welcome to DWR! Create a custom flag seed with !dwflags <flags> or !dwflags3 <flags> for version 3.0'
                 )
             await self.send_message(
                 'Full list of raceroom commands at https://pastebin.com/raw/4nKVRxXR'
@@ -34,7 +34,6 @@ class RandoHandler(RaceHandler):
             self.state['locked'] = False
 
         self.state['seed_rolled'] = False
-        self.state['race_version'] = 'v2.2.1'
 
     @monitor_cmd
     async def ex_lock(self, args, message):
@@ -68,7 +67,19 @@ class RandoHandler(RaceHandler):
         """
         if self._race_in_progress():
             return
+        if not self.state['race_version']:
+            self.state['race_version'] = 'v2.2.1'
         await self.roll_and_send(args, message)
+
+    async def ex_dwflags3(self, args, message):
+        """
+        Handle !dwflags3 commands.
+        """
+        if self._race_in_progress():
+            return
+        if not self.state['race_version']:
+            self.state['race_version'] = 'v3.0'
+        await self.roll_and_send_v3(args, message)
 
     async def ex_version(self, args, message):
         """
@@ -87,7 +98,11 @@ class RandoHandler(RaceHandler):
             await self.send_message('Versions must start with "v" (ex.: "v2.2")')
             return
         self.state['race_version'] = version
+        await self.send_message('Seed version updated to: {}'.format(version))
         await self.update_info()
+
+    async def ex_url(self, args, message):
+        await self.print_url()
 
     async def ex_clear(self, args, message):
         """
@@ -110,8 +125,55 @@ class RandoHandler(RaceHandler):
         else:
             await self.send_message('This command only works in Standard and Tournament')
 
+    async def ex_roll3(self, args, message):
+        """
+        Rolls a new seed with the room default flags for version 3.0.
+        """
+        reply_to = message.get('user', {}).get('name')
+        
+        goal_name = self.data.get('goal', {}).get('name')
+        if (goal_name == 'Standard Flags' or goal_name == 'Tournament'):
+            self.state['race_version'] = 'v3.0'
+            await self.roll(
+                flags="IVIAAVCAKACAAAAAAAAAAEAA",
+                reply_to=reply_to,
+            )
+        else:
+            await self.send_message('This command only works in Standard and Tournament')
+
 
     async def roll_and_send(self, args, message):
+        """
+        Read an incoming !dwflags command, and generate a new seed if
+        valid.
+        """
+        reply_to = message.get('user', {}).get('name')
+
+        if len(args) != 1:
+            await self.send_message('Hey, you forgot flags.')
+            return
+        if self.state.get('locked') and not can_monitor(message):
+            await self.send_message(
+                'Sorry %(reply_to)s, seed rolling is locked. Only race '
+                'monitors may roll a seed for this race.'
+                % {'reply_to': reply_to or 'friend'}
+            )
+            return
+        if self.state.get('seed_rolled') and not can_moderate(message):
+            await self.send_message(
+                'Well excuuuuuse me princess, but I already rolled a seed. '
+                'Don\'t get greedy!'
+            )
+            return
+
+        words = message.get('message', '').split(' ')
+
+        await self.roll(
+            flags=words[1],
+            reply_to=reply_to,
+        )
+
+    async def roll_and_send_v3(self, args, message):
         """
         Read an incoming !dwflags command, and generate a new seed if
         valid.
@@ -161,7 +223,7 @@ class RandoHandler(RaceHandler):
         self.state['seed_rolled'] = False
         self.state['race_flagstring'] = ''
         self.state['race_seed'] = 0
-        self.state['version'] = 'v2.2.1'
+        self.state['race_version'] = ''
         await self.send_message('Race info cleared!')
 
     async def update_info(self):
@@ -175,6 +237,13 @@ class RandoHandler(RaceHandler):
                 self.state['race_version'], 
                 self.state['race_seed'],
                 self.state['race_flagstring']))
+            await self.print_url()
+
+    async def print_url(self):
+        if (self.state['seed_rolled'] and self.state['race_version'].startswith('v3.0')):
+            await self.send_message('https://dwrandomizer.com/release/#flags={}&seed={}'.format(
+                self.state['race_flagstring'], 
+                self.state['race_seed']))
 
     def _race_in_progress(self):
         return self.data.get('status').get('value') in ('pending', 'in_progress')
