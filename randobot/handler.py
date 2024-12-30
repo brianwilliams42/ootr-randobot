28ebf610c507d024c885cc0fd02e301e4e77408c
Brian Williams <brianwilliams42@gmail.com>
2024-12-30 01:21:05 +0000

    Update handler.py

--- randobot/handler.py
+++ randobot/handler.py
@@ -157,7 +157,7 @@ class RandoHandler(RaceHandler):
         """
         Rolls a new seed with the week 1 2024 winter league flags.
         """
-               await self.roll3nonstandard('CVIQAVCFKACBAQAAIAAAA2QQ', 'Willkommen in der Winterliga', args, message)
+        await self.roll3nonstandard('CVIQAVCFKACBAQAAIAAAA2QQ', 'Willkommen in der Winterliga', args, message)
 
     async def ex_week2(self, args, message):
         """
