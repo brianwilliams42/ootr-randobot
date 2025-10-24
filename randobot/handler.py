20b9a2490b2c7f5b472dae90681eaf0e847de343
François <juef17@users.noreply.github.com>
2025-10-23 21:59:12 -0400

    Updated DWR-juef version to v3.0.3.20

--- randobot/handler.py
+++ randobot/handler.py
@@ -205,7 +205,7 @@ class RandoHandler(RaceHandler):
             await self.send_message('This command does not work in Standard or Tournament')
             return
         else:
-            self.state['race_version'] = 'juef-v3.0.3.19'
+            self.state['race_version'] = 'juef-v3.0.3.20'
             self.state['build_type'] = 'juef'
             await self.roll_and_send_v3(args, message)
 
